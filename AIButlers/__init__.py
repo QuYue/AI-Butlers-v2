da8a36c1071939cfe22a26f30b1f25ce1fef4136
quyue <quyue1541@gmail.com>
2023-02-22 02:50:26 +0800

    Add get map

--- AIButlers/__init__.py
+++ AIButlers/__init__.py
@@ -8,22 +8,29 @@ Introduction:
 '''
 #%% Import Packages
 # Basic
+import os
+import sys
 import json
+
+# Add Path
+if __package__ is None:
+    os.chdir(os.path.dirname(__file__))
+    sys.path.append('..')
 
 # Self-defined
 import utils
-from . import butler
+# from . import butler
 
 #%% Functions
 def read_config(path):
-    new_config = utils.MyStruct()
+    config = utils.MyStruct()
     with open(path, encoding="utf-8") as f:
         d = f.read()
         d = json.loads(d)
-        new_config.add_json(d)
-    return new_config
+        config.add_json(d)
+    return config
 
-def get_map(config_path):
+def get_map(config):
     map = utils.MyStruct()
     map.robot = utils.MyStruct()
     map.user = utils.MyStruct()
@@ -32,9 +39,33 @@ def get_map(config_path):
     map.robot.code2name = dict()
 
     map.user.name2id = dict()
-    map.user.name2id = dict()
+    map.user.id2name = dict()
+    map.user.cname2id = dict()
+    map.user.id2cname = dict()
+
+    for user in config.dingtalk.users.dict:
+        uid = config.dingtalk.users.dict[user].id
+        ucname = config.dingtalk.users.dict[user].cname
+        map.user.name2id[user] = uid
+        map.user.id2name[uid] = user
+        map.user.cname2id[ucname] = uid
+        map.user.id2cname[uid] = ucname
+
+    for robot in config.dingtalk.robots.dict:
+        robotcode = config.dingtalk.robots.dict[robot].robotCode
+        map.robot.name2code[robot] = robotcode
+        map.robot.code2name[robotcode] = robot
+
+    config.map = map
 
 
+#%% Main Function
+if __name__ == "__main__":
+    config = read_config("../config.json")
+    get_map(config)
+    print(config.map.user.id2cname['manager1393'])
         
 
 
+
+# %%
