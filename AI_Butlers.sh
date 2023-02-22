20f06c33d8f960d07b2d069bc60f53026255e0c6
QuYue <quyue1541@gmail.com>
2023-02-23 02:44:28 +0800

    resave

--- AI_Butlers.sh
+++ AI_Butlers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'starting project'
-source /root/Software/miniconda3/etc/profile.d/conda.sh
-conda activate AI
-cd /root/Files/AI-Butlers-v2
+echo 'Starting AI-Butlers-v2'
+source /root/Softwares/miniconda3/etc/profile.d/conda.sh
+conda activate server
+cd /root/Projects/AI-Butlers-v2
 python ./AI_Butlers.py -p "./config.json"
